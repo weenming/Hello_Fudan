6b717cc72bfe84fc8824d7cf4d034c7c8c8ac471
Weenming <96657222+weenming@users.noreply.github.com>
2022-11-27 11:34:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,23 @@ from PIL import ImageEnhance
 from requests import session, post, adapters
 adapters.DEFAULT_RETRIES = 5
 
+
+class CustomHttpAdapter (requests.adapters.HTTPAdapter):
+    '''Transport adapter" that allows us to use custom ssl_context.'''
+
+    def __init__(self, ssl_context=None, **kwargs):
+        self.ssl_context = ssl_context
+        super().__init__(**kwargs)
+
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = urllib3.poolmanager.PoolManager(
+            num_pools=connections, maxsize=maxsize,
+            block=block, ssl_context=self.ssl_context)
+        
+ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+ctx.options |= 0x4
+session.mount('https://', CustomHttpAdapter(ctx))
+
 class Fudan:
     """
     建立与复旦服务器的会话，执行登录/登出操作
